ce8feaff87584f9b3ae014c431bc6012ac7152a5
collaborationteam1 <sebastian.hitzler@collaborationteam.one>
2025-05-08 22:10:52 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,2 +1,2 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:3001'); // Ändere diese URL nach dem Deployment
+export const socket = io('https://team-game.onrender.com'); // Ändere diese URL nach dem Deployment
